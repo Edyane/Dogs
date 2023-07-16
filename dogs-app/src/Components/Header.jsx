74db0d827515effec7fa18b955ac2391fa9e8114
Edyane <edyanealves@gmail.com>
2023-07-15 22:28:42 -0300

    feat: adicionando links ao componente Header

--- dogs-app/src/Components/Header.jsx
+++ dogs-app/src/Components/Header.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/login">Login/Criar</Link>
+      </nav>
+    </div>
   )
 }
 
