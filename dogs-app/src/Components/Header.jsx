2758d40782289d32968b81d15b9f6cff567b9d2f
Edyane <edyanealves@gmail.com>
2023-07-15 22:29:18 -0300

    style: ajustando código

--- dogs-app/src/Components/Header.jsx
+++ dogs-app/src/Components/Header.jsx
@@ -3,14 +3,14 @@ import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
-  return (
-    <div>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login">Login/Criar</Link>
-      </nav>
-    </div>
-  )
+	return (
+		<div>
+			<nav>
+				<Link to="/">Home</Link>
+				<Link to="/login">Login/Criar</Link>
+			</nav>
+		</div>
+	)
 }
 
 export default Header
