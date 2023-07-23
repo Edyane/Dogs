98fa8d79cf94dbd161778f3ec4e488bca28e6a73
Edyane <edyanealves@gmail.com>
2023-07-23 20:50:08 -0300

    fix: resolvendo bugs

--- dogs-app/src/Components/Header.jsx
+++ dogs-app/src/Components/Header.jsx
@@ -5,17 +5,17 @@ import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 
 const Header = () => {
 	return (
-		<div className={styles.header}>
+		<header className={styles.header}>
 			<nav className={`${styles.nav} container`}>
 				<Link className={styles.logo} to="/" aria-label="Dogs - Home">
 					<Dogs />
 				</Link>
 				<Link className={styles.login} to="/login">
-					Login/Criar
+					Login / Criar
 				</Link>
 			</nav>
-		</div>
-	)
-}
+		</header>
+	);
+};
 
-export default Header
+export default Header;
