2962f408a0e86be06b0446130ba1ca112abbee8b
Edyane <edyanealves@gmail.com>
2023-07-24 18:35:44 -0300

    style: identando código

--- dogs-app/src/App.jsx
+++ dogs-app/src/App.jsx
@@ -7,18 +7,18 @@ import Home from './Components/Home';
 import Login from './Components/Login/Login';
 
 function App() {
-  return (
-    <div>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+				<Footer />
+			</BrowserRouter>
+		</div>
+	);
 }
 
 export default App;
