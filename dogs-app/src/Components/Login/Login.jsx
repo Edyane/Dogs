92501c078fe442e4f54850be61ea1116d2c3801d
Edyane <edyanealves@gmail.com>
2023-07-25 22:27:40 -0300

    feat: nested routes

--- dogs-app/src/Components/Login/Login.jsx
+++ dogs-app/src/Components/Login/Login.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
 const Login = () => {
   return (
-    <div>Login</div>
+   <Routes>
+    <Route path="/" element={<LoginForm />} />
+    <Route path="criar" element={<LoginCreate />} />
+    <Route path="perdeu" element={<LoginPasswordLost />} />
+    <Route path="resetar" element={<LoginPasswordReset />} />
+   </Routes>
   )
 }
 
