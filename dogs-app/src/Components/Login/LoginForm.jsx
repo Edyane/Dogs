dde781936f48e1846d1c81fb3389e9cb368459a3
Edyane <edyanealves@gmail.com>
2023-07-29 20:23:59 -0300

    feat: adicionando inputs para username e password

--- dogs-app/src/Components/Login/LoginForm.jsx
+++ dogs-app/src/Components/Login/LoginForm.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
-	const [username, setUsername] = useState('');
+	const [username, setUsername] = useState(''),
 		[password, setPassword] = useState('');
-		
+
 	return (
 		<section>
 			<h1>Login</h1>
-			<form action="">
-				<input type="text"/>
+
+			{/* onSubmit = ao enviar eu quero que ocorra uma ação */}
+			<form action="" >
+				<input type="text"
+					value={username}
+					onChange={({ target }) => setUsername(target.value)} />
+
+				<input type="text"
+					value={password}
+					onChange={({ target }) => setPassword(target.value)} />
+
+				<button>Entrar</button>
 			</form>
+
 			<Link to="/login/criar">Cadastro</Link>
 		</section>
 	)
