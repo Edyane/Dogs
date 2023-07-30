50eea6d511c472083006fd87ea0f63198742d849
Edyane <edyanealves@gmail.com>
2023-07-29 21:36:59 -0300

    feat: adicionando handleSumbit function

--- dogs-app/src/Components/Login/LoginForm.jsx
+++ dogs-app/src/Components/Login/LoginForm.jsx
@@ -1,30 +1,42 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Input from '../Form/Input';
+import Button from '../Form/Button';
 
 const LoginForm = () => {
 	const [username, setUsername] = useState(''),
 		[password, setPassword] = useState('');
 
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ username, password }),
+		}).then((response) => {
+			console.log(response);
+			return response.json();
+		}).then(json => {
+			console.log(json);
+		})
+	}
+
 	return (
 		<section>
 			<h1>Login</h1>
-
 			{/* onSubmit = ao enviar eu quero que ocorra uma ação */}
-			<form action="" >
-				<input type="text"
-					value={username}
-					onChange={({ target }) => setUsername(target.value)} />
-
-				<input type="text"
-					value={password}
-					onChange={({ target }) => setPassword(target.value)} />
-
-				<button>Entrar</button>
+			
+			<form action="" onSubmit={handleSubmit}>
+				<Input label="Usuário" type="text" name={username} />
+				<Input label="Senha" type="password" name={password} />
+				<Button>Entrar</Button>
 			</form>
 
 			<Link to="/login/criar">Cadastro</Link>
 		</section>
-	)
-}
+	);
+};
 
 export default LoginForm;
