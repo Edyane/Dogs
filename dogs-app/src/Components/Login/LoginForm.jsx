495d70d1373bababb5f9ddd16746336784744f31
Edyane <edyanealves@gmail.com>
2023-08-02 22:48:34 -0300

    fix: ajustando componente LoginForm

--- dogs-app/src/Components/Login/LoginForm.jsx
+++ dogs-app/src/Components/Login/LoginForm.jsx
@@ -5,40 +5,32 @@ import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
-	const username = useForm('email');	
+	const username = useForm();
 	const password = useForm();
-	console.log(password.value);
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json',
-			},
-			body: JSON.stringify(),
-		}).then((response) => {
-			console.log(response);
-			return response.json();
-		}).then(json => {
-			console.log(json);
-		})
+	async function handleSubmit(event) {
+		event.preventDefault();
+
+		if (username.validate() && password.validate()) {
+			userLogin(username.value, password.value);
+		}
 	}
 
 	return (
 		<section>
 			<h1>Login</h1>
-			{/* onSubmit = ao enviar eu quero que ocorra uma ação */}
-			
-			<form action="" onSubmit={handleSubmit}>
-				<Input label="Usuário" type="text" name={...username} />
-				<Input label="Senha" type="password" name={...password} />
-				<Button>Entrar</Button>
+			<form onSubmit={handleSubmit}>
+				<Input label="Usuário" type="text" name="username" {...username} />
+				<Input label="Senha" type="password" name="password" {...password} />
 			</form>
-
-			<Form />
-
-			<Link to="/login/criar">Cadastro</Link>
+			<Link to="/login/perdeu">
+				Perdeu a Senha?
+			</Link>
+			<div>
+				<Link to="/login/criar">
+					Cadastro
+				</Link>
+			</div>
 		</section>
 	);
 };
