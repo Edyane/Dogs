1083ee16a554422a3dad03f6d64387b95502321d
Edyane <edyanealves@gmail.com>
2023-08-02 22:37:47 -0300

    feat: adicionando verificações para a senha e o número

--- dogs-app/src/Hooks/useForm.jsx
+++ dogs-app/src/Hooks/useForm.jsx
@@ -5,6 +5,15 @@ const types = {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um email válido',
     },
+    password: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+        message:
+            'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
+    },
+    number: {
+        regex: /^\d+$/,
+        message: 'Utilize números apenas.',
+    },
 };
 
 const useForm = (type) => {
@@ -16,6 +25,12 @@ const useForm = (type) => {
         if (value.length === 0) {
             setError('Preencha um valor.');
             return false;
+        } else if (types[type] && !types[type].regex.test(value)) {
+            setError(types[type].message);
+            return false;
+        } else {
+            setError(null);
+            return true;
         }
     }
 
@@ -29,6 +44,8 @@ const useForm = (type) => {
         setValue,
         onChange,
         error,
+        validate: () => validate(value),
+        onBlur: () => validate(value),
     };
 };
 
