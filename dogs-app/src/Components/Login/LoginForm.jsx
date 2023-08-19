b195b26b55aefddfe3c23aaddfc6cee69cd4e7ad
Edyane <edyanealves@gmail.com>
2023-08-18 22:46:01 -0300

    fix: recomeçando LoginForm

--- dogs-app/src/Components/Login/LoginForm.jsx
+++ dogs-app/src/Components/Login/LoginForm.jsx
@@ -5,34 +5,7 @@ import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
-	const username = useForm();
-	const password = useForm();
-
-	async function handleSubmit(event) {
-		event.preventDefault();
-
-		if (username.validate() && password.validate()) {
-			userLogin(username.value, password.value);
-		}
-	}
-
-	return (
-		// <section>
-		// 	<h1>Login</h1>
-		// 	<form onSubmit={handleSubmit}>
-		// 		<Input label="Usuário" type="text" name="username" {...username} />
-		// 		<Input label="Senha" type="password" name="password" {...password} />
-		// 	</form>
-		// 	<Link to="/login/perdeu">
-		// 		Perdeu a Senha?
-		// 	</Link>
-		// 	<div>
-		// 		<Link to="/login/criar">
-		// 			Cadastro
-		// 		</Link>
-		// 	</div>
-		// </section>
-	);
+	
 };
 
 export default LoginForm;
