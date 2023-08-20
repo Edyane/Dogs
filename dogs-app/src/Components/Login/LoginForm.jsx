3e86cc63328a3463d56e3a3401964a5a226d1b0a
Edyane <edyanealves@gmail.com>
2023-08-20 17:15:57 -0300

    feat: transformando inputs em componentes

--- dogs-app/src/Components/Login/LoginForm.jsx
+++ dogs-app/src/Components/Login/LoginForm.jsx
@@ -5,42 +5,34 @@ import Button from '../Form/Button';
 import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
-	const [username, setUsername] = useState('');
+    const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
 
     function handleSubmit(event) {
         event.preventDefault();
         fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-           method: 'POST',
-           headers: {
-            'Content-Type' : 'application/json',
-           },
-           body: JSON.stringify({ username, password}),
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ username, password }),
         })
-        .then((response) => {
-            console.log(response);
-            return response.json();
-        })
-        .then((json) => {
-            console.log(json);
-        });
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((json) => {
+                console.log(json);
+            });
     }
 
     return (
         <section>
             <h1>Login</h1>
             <form action="" onSubmit={handleSubmit}>
-                <input
-                    type="text"
-                    value={username}
-                    onChange={({ target }) => setUsername(target.value)}
-                />
-                <input
-                    type="text"
-                    value={password}
-                    onChange={({ target }) => setPassword(target.value)}
-                />
-                <button>Entrar</button>
+                <Input label="Usuário" type="text" name="username" />
+                <Input label="Senha" type="password" name="password" />
+                <Button>Entrar</Button>
             </form>
             <Link to="/login/criar">Cadastro</Link>
         </section>
