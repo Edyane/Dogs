15efff090be354a7e4855fd657d68a54bf28bea6
Edyane <edyanealves@gmail.com>
2023-09-02 18:56:24 -0300

    fix: ajustando UserContext.jsx

--- dogs-app/src/UserContext.jsx
+++ dogs-app/src/UserContext.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
+import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './Api';
 import { useNavigate } from 'react-router-dom';
-import { TOKEN_POST, USER_GET } from './api';
 
 export const UserContext = React.createContext();
 
