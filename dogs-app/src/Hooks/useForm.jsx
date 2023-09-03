f978bc93e443d5ac47688b7704729c602e902aca
Edyane <edyanealves@gmail.com>
2023-09-03 19:13:26 -0300

    feat: adicionando verificação para senha

--- dogs-app/src/Hooks/useForm.jsx
+++ dogs-app/src/Hooks/useForm.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 const types = {
     email: {
         regex: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
         message: 'Preencha um email válido',
     },
+    password: {
+        regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+        message:
+            'A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.',
+    },
 };
 
 const useForm = (type) => {
-    const [value, setValue] = useState('');
-    const [error, setError] = useState(null);
+    const [value, setValue] = React.useState('');
+    const [error, setError] = React.useState(null);
 
     function validate(value) {
         if (type === false) return true;
@@ -25,7 +30,7 @@ const useForm = (type) => {
         }
     }
 
-    const onChange = ({ target }) => {
+    function onChange({ target }) {
         if (error) validate(target.value);
         setValue(target.value);
     }
