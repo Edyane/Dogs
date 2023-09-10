c9b81451b2d875e3cad0f94e64e3e567022df282
Edyane <edyanealves@gmail.com>
2023-09-09 22:56:00 -0300

    feat: adicionando funcionalidades

--- dogs-app/src/Components/Forms/Input.jsx
+++ dogs-app/src/Components/Forms/Input.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
 import styles from './Input.module.css';
 
-const Input = ({ label, type, name, value, onChange }) => {
-    return (
-        <div className={styles.wrapper}>
-            <label htmlFor={name} className={styles.wrapper}>{label}</label>
-            <input className={styles.input} name={name} type={type} value={value} onChange={onChange} />
-            <p className={styles.error}>Erro</p>
-        </div>
-    )
-}
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
+  return (
+    <div className={styles.wrapper}>
+      <label htmlFor={name} className={styles.label}>
+        {label}
+      </label>
+      <input
+        id={name}
+        name={name}
+        className={styles.input}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
+    </div>
+  );
+};
 
 export default Input;
