b3c6006ba2f8e6876c87c02270da789c784c7e8b
Edyane <edyanealves@gmail.com>
2023-09-14 21:49:57 -0300

    feat: adicionando rotas em UserHeaderNav

--- dogs-app/src/Components/User/UserHeaderNav.jsx
+++ dogs-app/src/Components/User/UserHeaderNav.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { NavLink, useNavigate } from 'react-router-dom';
+import { UserContext } from '../../UserContext';
+import { ReactComponent as MinhasFotos } from '../../Assets/feed.svg';
+import { ReactComponent as Estatisticas } from '../../Assets/estatisticas.svg';
+import { ReactComponent as AdicionarFoto } from '../../Assets/adicionar.svg';
+import { ReactComponent as Sair } from '../../Assets/sair.svg';
+import styles from './UserHeaderNav.module.css';
 
 const UserHeaderNav = () => {
-    const { userLogout } = React.useContext(UserContext);
+    const { userLogout } = useContext(UserContext);
     const navigate = useNavigate();
     function handleLogout() {
         userLogout();
         navigate('/login');
     }
-    const [mobile, setMobile] = React.useState(null);
+    const [mobile, setMobile] = useState(null);
 
     return (
         <nav className={styles.nav}>
+            <NavLink to="/conta" end>
+                <MinhasFotos />
+                {mobile && 'Minhas Fotos'}
+            </NavLink>
+            <NavLink to="/conta/estatisticas">
+                <Estatisticas />
+                {mobile && 'Estatísticas'}
+            </NavLink>
+            <NavLink to="/conta/postar">
+                <AdicionarFoto />
+                {mobile && 'Adicionar Foto'}
+            </NavLink>
+            <button onClick={handleLogout}>
+                <Sair />
+                {mobile && 'Sair'}
+            </button>
         </nav>
     );
 };
