86fdcc608a2af002afb56856d60ce66518a9d962
Edyane <edyanealves@gmail.com>
2023-10-05 22:12:11 -0300

    fix: adicionando pequenos ajustes ao código

--- dogs-app/src/Components/Photo/PhotoComments.jsx
+++ dogs-app/src/Components/Photo/PhotoComments.jsx
@@ -1,25 +1,25 @@
-import React, { useContext } from 'react';
+import React, { useState, useContext } from 'react';
 import { UserContext } from '../../UserContext';
 import PhotoCommentsForm from './PhotoCommentsForm';
 import styles from './PhotoComments.module.css';
 
 const PhotoComments = (props) => {
-  const [comments, setComments] = React.useState(() => props.comments);
-  const { login } = useContext(UserContext);
+	const [comments, setComments] = useState(() => props.comments);
+	const { login } = useContext(UserContext);
 
-  return (
-    <>
-      <ul className={styles.comments}>
-        {comments.map((comment) => (
-          <li key={comment.comment_ID}>
-            <b>{comment.comment_author}: </b>
-            <span>{comment.comment_content}</span>
-          </li>
-        ))}
-      </ul>
-      {login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
-    </>
-  );
+	return (
+		<>
+			<ul className={styles.comments}>
+				{comments.map((comment) => (
+					<li key={comment.comment_ID}>
+						<b>{comment.comment_author}: </b>
+						<span>{comment.comment_content}</span>
+					</li>
+				))}
+			</ul>
+			{login && <PhotoCommentsForm id={props.id} setComments={setComments} />}
+		</>
+	);
 };
 
 export default PhotoComments;
