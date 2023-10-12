8924a5387ffa7e7b7ca7bf23b8f3f42e81ce45fb
Edyane <edyanealves@gmail.com>
2023-10-12 09:13:47 -0300

    fix: ajustando FeedPhotos

--- dogs-app/src/Components/Feed/FeedPhotos.jsx
+++ dogs-app/src/Components/Feed/FeedPhotos.jsx
@@ -7,32 +7,33 @@ import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
 const FeedPhotos = ({ setModalPhoto }) => {
-    const { data, loading, error, request } = useFetch();
+	const { data, loading, error, request } = useFetch();
 
-    useEffect(() => {
-        async function fetchPhotos() {
-            const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
-            const { json } = await request(url, options);
-            console.log(json);
-        }
-        fetchPhotos();
-    }, [request]);
+	useEffect(() => {
+		async function fetchPhotos() {
+			const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+			const { json } = await request(url, options);
+			console.log(json);
+		}
+		fetchPhotos();
+	}, [request]);
 
-    if (error) return <Error error={error} />;
-    if (loading) return <Loading />;
-    if (data)
-        return (
-            <ul className={`${styles.feed} animeLeft`}>
-                {data.map((photo) => (
-                    <FeedPhotosItem
-                        key={photo.id}
-                        photo={photo}
-                        setModalPhoto={setModalPhoto}
-                    />
-                ))}
-            </ul>
-        );
-    else return null;
+	if (error) return <Error error={error} />;
+	if (loading) return <Loading />;
+	if (data)
+		return (
+			<ul className={`${styles.feed} animeLeft`}>
+				{data.map((photo) => (
+					<FeedPhotosItem
+						key={photo.id}
+						photo={photo}
+						setModalPhoto={setModalPhoto}
+					/>
+				))}
+			</ul>
+		);
+	else return null;
 };
 
 export default FeedPhotos;
+
