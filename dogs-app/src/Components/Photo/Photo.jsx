63fe05b40fe7a599db92b2dda1e4d2750149cb12
Edyane <edyanealves@gmail.com>
2023-10-29 22:30:42 -0300

    fix: ajustando react hooks

--- dogs-app/src/Components/Photo/Photo.jsx
+++ dogs-app/src/Components/Photo/Photo.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import useFetch from '../../Hooks/useFetch';
 import { PHOTO_GET } from '../../Api';
 import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import PhotoContent from './PhotoContent';
+import { useEffect } from 'react';
 
 const Photo = () => {
 	const { id } = useParams();
 	const { data, loading, error, request } = useFetch();
 
-	React.useEffect(() => {
+	useEffect(() => {
 		const { url, options } = PHOTO_GET(id);
 		request(url, options);
 	}, [request, id]);
