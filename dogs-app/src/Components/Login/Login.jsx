a912b772a802422fb164af26b5b50b9bff4a457f
Edyane <edyanealves@gmail.com>
2023-11-05 18:53:21 -0300

    feat: adicionando rota de página não encontrada ao login

--- dogs-app/src/Components/Login/Login.jsx
+++ dogs-app/src/Components/Login/Login.jsx
@@ -5,6 +5,7 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../UserContext';
+import NotFound from '../../Components/NotFound';
 import styles from './Login.module.css';
 
 const Login = () => {
@@ -19,6 +20,7 @@ const Login = () => {
                     <Route path="criar" element={<LoginCreate />} />
                     <Route path="perdeu" element={<LoginPasswordLost />} />
                     <Route path="resetar" element={<LoginPasswordReset />} />
+                    <Route path="*" element={<NotFound />} />
                 </Routes>
             </div>
         </section>
