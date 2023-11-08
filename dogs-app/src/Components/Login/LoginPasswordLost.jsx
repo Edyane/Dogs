2aec35212dcdef2e9bcf6a7aa36c9aef1a98f9bc
Edyane <edyanealves@gmail.com>
2023-11-07 22:23:32 -0300

    feat: construindo página de 'perdi minha senha'

--- dogs-app/src/Components/Login/LoginPasswordLost.jsx
+++ dogs-app/src/Components/Login/LoginPasswordLost.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import useFetch from '../../Hooks/useFetch';
+import { PASSWORD_LOST } from '../../Api';
+import Error from '../Helper/Error';
 
 const LoginPasswordLost = () => {
+	const login = useForm();
+	const { data, loading, error, request } = useFetch();
+
 	return (
-		<div>LoginPasswordLost</div>
-	)
-}
+		<section>
+			<h1 className="title">Perdeu a senha?</h1>
+		</section>
+	);
+};
 
 export default LoginPasswordLost;
