06e398c9e9916e183a78944192f686c8811751c0
Edyane <edyanealves@gmail.com>
2023-12-21 22:27:28 -0300

    refactor: ajustando react hooks

--- dogs-app/src/Components/Feed/Feed.jsx
+++ dogs-app/src/Components/Feed/Feed.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FeedModal from './FeedModal';
 import FeedPhotos from './FeedPhotos';
 import PropTypes from 'prop-types';
 
 const Feed = ({ user }) => {
-	const [modalPhoto, setModalPhoto] = React.useState(null);
-	const [pages, setPages] = React.useState([1]);
-	const [infinite, setInfinite] = React.useState(true);
+	const [modalPhoto, setModalPhoto] = useState(null);
+	const [pages, setPages] = useState([1]);
+	const [infinite, setInfinite] = useState(true);
 
-	React.useEffect(() => {
+	useEffect(() => {
 		let wait = false;
 		function infiniteScroll() {
 			if (infinite) {
